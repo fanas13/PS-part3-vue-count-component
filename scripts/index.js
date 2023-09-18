09b1a3c91251fda5c7ab935409714dab57b9112c
Nicholson Galela <nicholsongalela@gmail,com>
2023-09-18 16:43:55 +0200

    created methods to increase, decrease and highlight the text if minus/positive

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,33 @@
-Vue.component(counter-component), {
-    
-}
+Vue.component('counter-component', {
+    data() {
+        return {
+            count: 0
+        };
+    },
+    methods: {
+        increment() {
+            this.count++;
+        },
+        decrement() {
+            this.count--;
+        }
+    },
+    computed: {
+      countStyle() {
+        return {
+            color: this.count > 0 ? '#42a800' : this.count < 0 ? '#FF0000' : '#000000'
+        };
+      }
+    },
+    template: `
+        <div>
+            <p :style="countStyle">Current Count: {{ count }}</p>
+            <button @click="increment">Increment</button>
+            <button @click="decrement">Decrement</button>
+        </div>
+    `
+});
+
+new Vue({
+    el: '#app'
+});
